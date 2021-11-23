881665210fa03a33c126f3f44190267f6cf6d1ff
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-11-23 13:25:24 +0900

    Initial commit

--- src/javaScript2.js
+++ src/javaScript2.js
@@ -1,28 +1,28 @@
-class Animal {
-  constructor(type, neme, sound) {
-    this.type = type;
-    this.neme = neme;
-    this.sound = sound;
+class Food {
+  constructor(name) {
+    this.name = name;
+    this.brands = [];
   }
-  say() {
-    console.log(this.sound);
+  addBramd(brand) {
+    this.brands.push(brand);
   }
-}
-
-class Dag extends Animal {
-  constructor(name, sound) {
-    super("wofe", name, sound);
+  print() {
+    console.log(`${this.name} 을/를 파는 음식점들`);
+    console.log(this.brands.join(", "));
   }
 }
 
-class Cat extends Animal {
-  constructor(name, sound) {
-    super("Lion", name, sound);
-  }
-}
+const pizza = new Food("피자");
+pizza.addBramd("피자헛");
+pizza.addBramd("도미노 피자");
+pizza.addBramd("굽네 피자");
+pizza.addBramd("허리 피자");
 
-const dog = new Dag("멍멍이", "멍멍");
-const cat = new Cat("야옹이", "야옹");
+const chicken = new Food("치킨");
+chicken.addBramd("BBQ");
+chicken.addBramd("올리브영");
+chicken.addBramd("굽네치킨");
+chicken.addBramd("해태 용가리 치킨");
 
-dog.say;
-cat.say;
+pizza.print();
+chicken.print();
---
+++ src/javaScript3.js
@@ -0,0 +1,3 @@
+const array = [1, 2];
+let text = array.length === 0 ? "null" : "find";
+console.log(text);
