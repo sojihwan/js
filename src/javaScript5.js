1a2db4cfa7587e6aa148a993c7097dd73b67ea38
sojihwan <73238543+sojihwan@users.noreply.github.com>
2021-11-23 16:00:57 +0900

    Initial commit

--- src/javaScript5.js
+++ src/javaScript5.js
@@ -13,3 +13,11 @@ work((ms) => {
   console.log(ms + "작업이 끝났어요!");
 });
 console.log("next");
+//각 프로미스가 담긴 배열[]에
+//await Promise.all([])한번에 시작
+//await sleep(숫자(ms))
+//전채중 하나는 오류다  ==try 캐치문
+
+//Promise.race가장 빠른것이 결과물
+//첫번째가 하필 오류다 ==오류
+//두번째가 오류다 == 오류 없음
